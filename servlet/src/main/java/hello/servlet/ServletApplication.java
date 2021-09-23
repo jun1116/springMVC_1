2a19df91795a662f2e3adf7bed08c089bffe7e6c
hj <jun1057@ajou.ac.kr>
2021-09-23 14:56:45 +0900

    hello servlet start

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServletApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
-	}
 
+	}
 }
