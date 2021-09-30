304794437e9c326971fab1e46e8933001eeaed02
hj <jun1057@ajou.ac.kr>
2021-09-30 21:13:25 +0900

    mvc 패턴 적용 model : req.setAttribute() view : jsp controller : servlet

---
+++ servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,19 @@
+package hello.servlet.web.servletmvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";//이 경로안의 JSP를 컨트롤러를 통해 호출
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req,resp);//다른 서블릿이나 JSP로 이동할수있는 기능
+    }
+}
---
+++ servlet/src/main/webapp/WEB-INF/views/save-result.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hj
+  Date: 2021/09/30
+  Time: 9:01 오후
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+성공
+<ul>
+    <li>id=${member.id}</li>
+    <li>username=${member.username}</li>
+    <li>age=${member.age}</li>
+</ul>
+<a href="/index.html">메인으로</a>
+</body>
+</html>
