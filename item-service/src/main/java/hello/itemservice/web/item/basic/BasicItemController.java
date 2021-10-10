9fb12cbb26ed304fe553f63231d5776f90f28663
hj <jun1057@ajou.ac.kr>
2021-10-11 03:46:29 +0900

    상품수정완료

--- item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -75,8 +75,11 @@ public class BasicItemController {
         return "basic/editForm";
     }
 
-//    @PostMapping("/{itemId}/edit")
-
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
 
     /** For Testing Data **/
     @PostConstruct
