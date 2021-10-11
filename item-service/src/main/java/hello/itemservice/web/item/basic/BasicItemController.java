401b687da859412b4bb70c4abeb7d1b1ea4ed2a3
hj <jun1057@ajou.ac.kr>
2021-10-11 15:25:44 +0900

    PRG : Post, Redirect, Get RedirectAttributes 사용

--- item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -56,16 +57,30 @@ public class BasicItemController {
 //        model.addAttribute("item", item);//자동 추가, 생략 가능
         return "basic/item";
     }
-
 //    @PostMapping("/add")
+
     public String addItemV3(@ModelAttribute Item item, Model model){//Item -> item 의 이름으로 모델에 담기게돼!
         itemRepository.save(item);
         return "basic/item";
     }
-    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV4(Item item){//Item -> item 의 이름으로 모델에 담기게돼!
         itemRepository.save(item);
         return "basic/item";
+    }
+    /** PRG - Post/Redirect/Get **/
+//    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute("item") Item item){
+        itemRepository.save(item);
+        return "redirect:/basic/items/"+item.getId();
+    }
+    /** RedirectAttribute 사용 **/
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", item.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
